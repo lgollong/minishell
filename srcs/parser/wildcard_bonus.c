3c334ec17a1a632baf63c6c6033722d6f7c96f3d
Rasmus Wegat <rwegat@3-C-7.42heilbronn.de>
2024-11-14 15:42:32 +0100

    Wildcard up to norm

--- srcs/parser/wildcard_bonus.c
+++ srcs/parser/wildcard_bonus.c
@@ -6,66 +6,75 @@
 /*   By: rwegat <rwegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:47:55 by rwegat            #+#    #+#             */
-/*   Updated: 2024/11/14 12:34:50 by rwegat           ###   ########.fr       */
+/*   Updated: 2024/11/14 15:28:52 by rwegat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-// example a*middle_text*z*A*a
-int matchWildcard(char *filename, char *command)
+// special chars to interpret * as a literal char 
+void	handle_special_chars(char **wildcard, char **filename, \
+char **star, char **str)
 {
-	char *star = NULL;
-	char *str = NULL;
+	if (**wildcard == '*')
+	{
+		*star = *wildcard;
+		(*wildcard)++;
+		*str = *filename;
+		return ;
+	}
+	else if (**wildcard == '\\' && *(*wildcard + 1) == '*')
+	{
+		*wildcard += 2;
+		if (**filename != '*')
+			return ;
+		(*filename)++;
+	}
+	else if (**wildcard == '\'' && *(*wildcard + 1) == '*' \
+	&& *(*wildcard + 2) == '\'')
+	{
+		*wildcard += 3;
+		if (**filename != '*')
+			return ;
+		(*filename)++;
+	}
+}
 
+// compares the filename with the wildcard
+int	match_wildcard(char *filename, char *wildcard)
+{
+	char	*star;
+	char	*str;
+
+	str = NULL;
+	star = NULL;
 	while (*filename)
 	{
-		if (*command == '*')
+		if (*wildcard == '*' || *wildcard == '\'' || *wildcard == '\\')
+			handle_special_chars(&wildcard, &filename, &star, &str);
+		else if (*wildcard == *filename)
 		{
-			star = command++;
-			str = filename;
-		}
-		else if (*command == '\\' && *(command + 1) == '*')
-		{
-			command += 2;
-			if (*filename != '*')
-				return 0;
-			filename++;
-		}
-		else if (*command == '\'' && *(command + 1) == '*' && *(command + 2) == '\'')
-		{
-			command += 3;
-			if (*filename != '*')
-				return 0;
-			filename++;
-		}
-		else if (*command == *filename)
-		{
-			command++;
+			wildcard++;
 			filename++;
 		}
 		else if (star)
 		{
-			command = star + 1;
+			wildcard = star + 1;
 			filename = ++str;
 		}
 		else
-			return 0;
+			return (0);
 	}
-
-	while (*command == '*')
-		command++;
-
-	return (!*command);
+	while (*wildcard == '*')
+		wildcard++;
+	return (!*wildcard);
 }
 
-
-// Test function to demonstrate usage
 int	handle_wildcard(char *command, t_cmmnds *content)
 {
-	DIR *dir;
-	struct dirent *entry;
-	int match_found;
+	DIR				*dir;
+	struct dirent	*entry;
+	int				match_found;
 
 	match_found = 0;
 	if (!ft_strchr(command, '*'))
@@ -73,16 +82,18 @@ int	handle_wildcard(char *command, t_cmmnds *content)
 	dir = opendir(".");
 	if (!dir)
 		return (1);
-	while ((entry = readdir(dir)) != NULL)
+	entry = readdir(dir);
+	while (entry != NULL)
 	{
-		if (matchWildcard(entry->d_name, command))
+		entry = readdir(dir);
+		if (match_wildcard(entry->d_name, command))
 		{
 			cmmnd_to_struct(content, entry->d_name);
 			match_found = 1;
 		}
 	}
 	if (match_found == 0)
-		cmmnd_to_struct(content,command);
+		cmmnd_to_struct(content, command);
 	closedir(dir);
 	return (0);
 }
